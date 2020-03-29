1170b528685289cacad7350a77fb4de677c17a40
unknown <pashasimonpure@gmail.com>
2020-03-29 18:55:11 +0300

    welcome page fix

--- spring-boot-module/src/main/java/com/gmail/pashasimonpure/web/controller/WelcomeController.java
+++ spring-boot-module/src/main/java/com/gmail/pashasimonpure/web/controller/WelcomeController.java
@@ -1,27 +1,17 @@
 package com.gmail.pashasimonpure.web.controller;
 
-import java.util.List;
-
-import com.gmail.pashasimonpure.service.ItemService;
-import com.gmail.pashasimonpure.service.model.ItemDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
-    private final ItemService itemService;
-
-    public WelcomeController(ItemService itemService) {
-        this.itemService = itemService;
+    public WelcomeController() {
     }
 
     @GetMapping
-    public String welcomePage(Model model) {
-        List<ItemDTO> items = itemService.findAll();
-        model.addAttribute("items", items);
-        return "items";
+    public String welcomePage() {
+        return "redirect:items";
     }
 
 }
